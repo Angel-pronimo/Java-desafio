a2805c53156369d69abe717bca855e6d29c7821d
angel <paz.l.angel@gmail.com>
2025-04-19 14:21:37 -0300

    datos clientes

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,3 +1,13 @@
 public class Desafio {
+    public static void main(String[] args) {
+        String nombre = "Tony Stark";
+        String tipoDeCuenta = "corriente";
+        double saldo = 1599.99;
 
+        System.out.println("***************");
+        System.out.println("\n Nombre: " + nombre);
+        System.out.println("Tipo de cuenta: " + tipoDeCuenta);
+        System.out.println("Saldo: " + saldo); 
+
+}
 }
